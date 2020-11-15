484fc48398246e7c82c492358174c9ced5dfe428
Radosław Lipski <rlipski76@gmail.com>
2020-11-15 13:55:46 +0100

    logowanie, dodanie loginController

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdvertisementController;
+use App\Http\Controllers\api\mawa\LoginController;
 
-Route::post('login',[\Laravel\Passport\Http\Controllers\AccessTokenController::class, 'issueToken'])->middleware(['api-login', 'throttle']);
-//Route::get('users/list',[UserController::class, 'index']);
-Route::get('/advertisements', [AdvertisementController::class, 'index']);
-
-Route::post('/advertisement', [AdvertisementController::class, 'store']);
-
-Route::get('/advertisements/{id}', [AdvertisementController::class, 'show']);
-
-Route::put('/advertisements/{id}', [AdvertisementController::class, 'update']);
-
-Route::delete('/advertisements/{id}', [AdvertisementController::class, 'destroy']);
+Route::prefix('/user')->group(function() {
+  Route::post('/login', 'api\mawa\LoginController@login');
+});
